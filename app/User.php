1539aa5600b22282ae2bb773985319bbb28410f8
anindyaasalsa <anindya_salsabilla_27rpl@student.smktelkom-mlg.sch.id>
2020-03-24 22:30:27 +0700

    revisi login

--- app/User.php
+++ app/User.php
@@ -5,7 +5,6 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
@@ -38,11 +37,13 @@ class User extends Authenticatable implements JWTSubject
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-    public function getJWTIdentifier(){
+    public function getJWTIdentifier()
+    {
         return $this->getKey();
     }
-    public function getJWTCustomCaims(){
+
+    public function getJWTCustomClaims()
+    {
         return [];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('register', 'LoginController@register');
+Route::post('login', 'LoginController@login');
+
+Route::middleware(['jwt.verify'])->group(function(){
+
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
